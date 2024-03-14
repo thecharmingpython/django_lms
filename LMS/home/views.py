0219308460fcad27e0fe355e76cff2a1cd1d1621
Oz Rhodes <thecharmingpython@gmail.com>
2024-03-14 19:51:30 +0000

    Added home template

---
+++ LMS/home/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+
+    return render(request, template_name='index.html')
+
+def about(request):
+
+    return HttpResponse('<h1>About Page</h1>')
